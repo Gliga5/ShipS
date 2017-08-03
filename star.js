8c0f1c52858d67be1deb61a3e6b3e29705852900
Gliga5 <lukagligoric5@gmail.com>
2017-08-03 21:26:26 +0200

    Update star.js

--- star.js
+++ star.js
@@ -1,6 +1,6 @@
 function Star(x, y) {
-    this.x = width/2;
-    this.y = width/2 - 50;
+    this.x = x;
+    this.y = y;
     this.r = 25;
     this.timer = 0;
     
@@ -12,10 +12,5 @@ function Star(x, y) {
         ellipse(this.x, this.y, this.r * 2, this.r * 2)
         fill(255);
         rect(this.x, this.y, random(0, 15), random(0, 15));
-        
-        if (this.timer = 1000) {
-            this.x = x;
-            this.y = y;
-        }
     }
 }
