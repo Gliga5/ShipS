8ce822603be3d387db49afddbc922ae2e09306b9
Gliga5 <lukagligoric5@gmail.com>
2017-08-05 21:12:09 +0200

    Update wave.js

--- wave.js
+++ wave.js
@@ -6,14 +6,16 @@ function Wave() {
     
     this.display = function() {
         textSize(24)
+        strokeWeight(5);
+        stroke(0);
         fill(255);
-        text("Wave: " + this.number, this.x, this.y)
+        text("Score: " + this.number, this.x, this.y)
     }
     this.update = function() {
         this.timer += 1;
-        if (this.timer == 1900) {
+        if (this.timer == 100) {
             this.timer = 0;
-            this.number += 1;
+            this.number += 2;
         }
     }
 }
