690ebf414d5314f925759b910a671582e04f3df7
Gliga5 <lukagligoric5@gmail.com>
2017-08-05 21:14:56 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup() {
     for (var i = 0; i < 10; i++) {
         stars[i] = new Star();
     }
-    for (var i = 0; i < 100; i++) {
+    for (var i = 0; i < 200; i++) {
         zvezde[i] = new Zvezde();
     }
     wave = new Wave();
